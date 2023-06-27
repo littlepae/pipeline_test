36d288c667be480c1073f18324eccf51b40f4197
littlepae <43944550+littlepae@users.noreply.github.com>
2023-06-27 13:25:02 +0700

    Add files via upload

--- local_module.py
+++ local_module.py
@@ -1,3 +1,10 @@
+import pandas as pd
+from clearml import StorageManager
 def test_print():
     print('use local module')
+    # df = StorageManager.get_local_copy(remote_url="https://github.com/littlepae/pipeline_test/blob/main/test_csv.csv")
+    # print(df)
+    df = pd.read_csv("./test_csv.csv")
+    print(df)
+    print(type(df))
     return
