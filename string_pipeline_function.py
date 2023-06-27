ededa2e44d1d03ed1700a1a9d7f9868cdb2da92e
littlepae <43944550+littlepae@users.noreply.github.com>
2023-06-27 16:20:16 +0700

    Update string_pipeline_function.py

--- string_pipeline_function.py
+++ string_pipeline_function.py
@@ -20,39 +20,40 @@ def post_execute_callback_example(a_pipeline, a_node):
     # if we need the actual executed Task: Task.get_task(task_id=a_node.executed)
     return
 
-def random_string(string_list):
-    import random
-    import string
-    import time
-    from local_module import test_print
-    from local_folder import function_in_folder
+# def random_string(string_list):
+#     import random
+#     import string
+#     import time
+#     from local_module import test_print
+#     from local_folder import function_in_folder
 
-    string_list = string_list
-    print("string_list")
-    print(string_list)
+#     string_list = string_list
+#     print("string_list")
+#     print(string_list)
 
-    random.seed(time.time())
+#     random.seed(time.time())
 
-    random_char = random.choices(string.ascii_lowercase)[0]
-    print("Random char : "+random_char)
+#     random_char = random.choices(string.ascii_lowercase)[0]
+#     print("Random char : "+random_char)
 
-    string_list = string_list+random_char
+#     string_list = string_list+random_char
 
-    test_print()
-    function_in_folder()
+#     test_print()
+#     function_in_folder()
 
-    print('artifacts of this task:')
-    print(string_list)
+#     print('artifacts of this task:')
+#     print(string_list)
 
-    # we are done
-    print('Task done')
-    return string_list
+#     # we are done
+#     print('Task done')
+#     return string_list
 
 
 # Connecting ClearML with the current pipeline,
 # from here on everything is logged automatically
 pipe = PipelineController(
-    name="String fuction pipeline", project="pipeline examples", version="0.0.1", add_pipeline_tags=False
+    name="String fuction pipeline", project="pipeline examples", version="0.0.1", add_pipeline_tags=False,
+    repo='https://github.com/littlepae/pipeline_test.git',
 )
 
 pipe.add_parameter(
@@ -65,78 +66,78 @@ pipe.set_default_execution_queue("default")
 pipeline_parameter = pipe.get_parameters()
 print(pipeline_parameter["start_list_of_sting"])
 
-pipe.add_function_step(
-    name='stage_1',
-    function=random_string,
-    function_kwargs=dict(string_list='${pipeline.start_list_of_sting}'),
-    function_return=['string_list'],
-    cache_executed_step=False,
-    repo='https://github.com/littlepae/pipeline_test.git',
-)
-
-pipe.add_function_step(
-    name='stage_2',
-    function=random_string,
-    function_kwargs=dict(string_list='${stage_1.string_list}'),
-    function_return=['string_list'],
-    cache_executed_step=False,
-    repo='https://github.com/littlepae/pipeline_test.git',
-)
-
-pipe.add_function_step(
-    name='stage_3',
-    function=random_string,
-    function_kwargs=dict(string_list='${stage_2.string_list}'),
-    function_return=['string_list'],
-    cache_executed_step=False,
-    repo='https://github.com/littlepae/pipeline_test.git',
-)
-
-pipe.add_function_step(
-    name='stage_4',
-    function=random_string,
-    function_kwargs=dict(string_list='${stage_3.string_list}'),
-    function_return=['string_list'],
-    cache_executed_step=False,
-    repo='https://github.com/littlepae/pipeline_test.git',
-)
-
-# pipe.add_step(
-#     name="stage_1",
-#     base_task_project="pipeline examples",
-#     base_task_name="random string and concat",
-#     parameter_override={"General/string_list": "${pipeline.start_list_of_sting}"},
+# pipe.add_function_step(
+#     name='stage_1',
+#     function=random_string,
+#     function_kwargs=dict(string_list='${pipeline.start_list_of_sting}'),
+#     function_return=['string_list'],
+#     cache_executed_step=False,
+#     repo='https://github.com/littlepae/pipeline_test.git',
 # )
 
-# pipe.add_step(
-#     name="stage_2",
-#     parents=["stage_1"],
-#     base_task_project="pipeline examples",
-#     base_task_name="random string and concat",
-#     parameter_override={
-#         "General/string_list": "${stage_1.artifacts.list_of_string.preview}",
-#     },
+# pipe.add_function_step(
+#     name='stage_2',
+#     function=random_string,
+#     function_kwargs=dict(string_list='${stage_1.string_list}'),
+#     function_return=['string_list'],
+#     cache_executed_step=False,
+#     repo='https://github.com/littlepae/pipeline_test.git',
 # )
 
-# pipe.add_step(
-#     name="stage_3",
-#     parents=["stage_2"],
-#     base_task_project="pipeline examples",
-#     base_task_name="random string and concat",
-#     parameter_override={
-#         "General/string_list": "${stage_2.artifacts.list_of_string.preview}",
-#     },
+# pipe.add_function_step(
+#     name='stage_3',
+#     function=random_string,
+#     function_kwargs=dict(string_list='${stage_2.string_list}'),
+#     function_return=['string_list'],
+#     cache_executed_step=False,
+#     repo='https://github.com/littlepae/pipeline_test.git',
 # )
 
-# pipe.add_step(
-#     name="stage_4",
-#     parents=["stage_3"],
-#     base_task_project="pipeline examples",
-#     base_task_name="random string and concat",
-#     parameter_override={
-#         "General/string_list": "${stage_3.artifacts.list_of_string.preview}",
-#     },
+# pipe.add_function_step(
+#     name='stage_4',
+#     function=random_string,
+#     function_kwargs=dict(string_list='${stage_3.string_list}'),
+#     function_return=['string_list'],
+#     cache_executed_step=False,
+#     repo='https://github.com/littlepae/pipeline_test.git',
 # )
+
+pipe.add_step(
+    name="stage_1",
+    base_task_project="pipeline examples",
+    base_task_name="random string and concat",
+    parameter_override={"General/string_list": "${pipeline.start_list_of_sting}"},
+)
+
+pipe.add_step(
+    name="stage_2",
+    parents=["stage_1"],
+    base_task_project="pipeline examples",
+    base_task_name="random string and concat",
+    parameter_override={
+        "General/string_list": "${stage_1.artifacts.list_of_string.preview}",
+    },
+)
+
+pipe.add_step(
+    name="stage_3",
+    parents=["stage_2"],
+    base_task_project="pipeline examples",
+    base_task_name="random string and concat",
+    parameter_override={
+        "General/string_list": "${stage_2.artifacts.list_of_string.preview}",
+    },
+)
+
+pipe.add_step(
+    name="stage_4",
+    parents=["stage_3"],
+    base_task_project="pipeline examples",
+    base_task_name="random string and concat",
+    parameter_override={
+        "General/string_list": "${stage_3.artifacts.list_of_string.preview}",
+    },
+)
 
 # print("${stage_3.artifacts.list_of_string.preview}")
 
